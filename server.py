5e1f968b362c0cbb7deb1ce0c62f723288b02d96
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-05-16 12:00:07 +0900

    tomando informacion de la api del blog, render  title, subtitle en el html,

--- server.py
+++ server.py
@@ -1,10 +1,38 @@
+from datetime import datetime
+from pprint import pprint
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+
+# URL where the information about our blog is available in JSON format
+url="https://api.npoint.io/d1d60738d43254255bdf"
+
+def current_date():
+    date=datetime.now()
+    return date
+
+  
+
+
+def get_data():
+    request= requests.get(url)
+    
+    request_json = request.json()
+    
+    pprint(request_json)
+
+    return request_json
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    
+    
+    my_data=get_data()
+    
+    return render_template("index.html", data=my_data, date=current_date())
 
 
 @app.route("/contact")
